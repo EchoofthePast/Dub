b35ff1db3261d082a9e0960291ace6d467b120c0
James <40529002+EchoofthePast@users.noreply.github.com>
2019-04-08 16:39:46 +0200

    Add files via upload

--- Dub.go
+++ Dub.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"html"
 	"html/template"
+	"net/http"
+	"os"
+	"strings"
+
+	//"time"
 
 	"github.com/echoofthepast/Dub/static/goget"
 )
-
 
 // Go Gophers out there will need a starting point premade for them having solved a thousand problems.
 
 func main() {
 
-	//type localhost:1337/home in your browsers address bar
-	http.HandleFunc("/home", HandleHome)
-
 	//type localhost:1337/index in your browsers address bar
-	http.HandleFunc("/index", HandleIndex)
+	http.HandleFunc("/index", indexHandler)
+	//type localhost:1337/input in your browsers address bar
+	http.HandleFunc("/input", inputHandler)
 
 	//parses the entire static folder and sub folders
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
@@ -35,8 +38,6 @@ func HandleHome(w http.ResponseWriter, r *http.Request) {
 
 }
 
-
-
 func blank() (data *goget.DataStruct) {
 
 	image1 := "static/images/Gopher.png"
@@ -44,17 +45,17 @@ func blank() (data *goget.DataStruct) {
 	name := "Gopher"
 	surname := "Google"
 	info := "The one who does the work is the Gopher"
+	id := "2000010101"
 
 	data = &goget.DataStruct{
 
 		Name:    name,
 		Surname: surname,
 		Both:    name + surname,
-		Url:     "",
 		Image1:  image1,
 		Image2:  image2,
 		Info:    info,
-
+		ID: 	 id,
 	}
 
 	return data
@@ -65,44 +66,41 @@ func inputHandler(w http.ResponseWriter, r *http.Request) {
 
 	r.ParseForm()
 
-	var namestring string
-	var snamestring string
-	var infostring string
-	var medicationstring string
-
-	fname := r.Form["fname"] //
-	sname := r.Form["sname"]
-	info := r.Form["info"]
-	medication := r.Form["medication"]
-
-	for index := 0; index > 1; index++ {
-
-		namestring = fname[index]
-		snamestring = sname[index]
-		infostring = info[index]
-		medicationstring = medication[index]
-
+	if r.Method == "GET" {
+		fmt.Fprintf(os.Stdout, "GET, %q", html.EscapeString(r.URL.Path))
+	} else if r.Method == "POST" {
+		fmt.Fprintf(os.Stdout, "POST, %q", html.EscapeString(r.URL.Path))
+	} else {
+		http.Error(w, "Invalid request method.", 405)
 	}
 
-	name := namestring
-	surname := snamestring
-	information := infostring
-	meds := medicationstring
+	method := r.Method
 
-	fmt.Println("Name", name)
-	fmt.Println("Surname", surname)
-	fmt.Println("Information", information)
-	fmt.Println("Medication", meds)
+	fmt.Println(" Method:", method)
 
-	//simply writing to file in the same format.
+	var key string
+
+	if r.Method == "POST" {
+
+		for k, v := range r.Form {
+			key = k
+			fmt.Println("key:", key)
+			fmt.Println("val:", strings.Join(v, ""))
+		}
+
+	} //end of if statement
 
 	tmpl := template.Must(template.ParseFiles("templates/input.html"))
 	tmpl.Execute(w, nil)
 
 }
 
+var call int
+
+//animation engine
+
 //HandleIndex is the Handler for the index file
-func HandleIndex(w http.ResponseWriter, r *http.Request) {
+func indexHandler(w http.ResponseWriter, r *http.Request) {
 
 	var data *goget.DataStruct
 
@@ -138,11 +136,7 @@ func HandleIndex(w http.ResponseWriter, r *http.Request) {
 
 				}
 
-				
-
 			}
-
-
 
 		}
 
