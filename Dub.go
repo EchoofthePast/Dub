a85100f83f7386462491993cb1a9257826d8e139
James <40529002+EchoofthePast@users.noreply.github.com>
2019-09-24 17:32:13 +0200

    Add files via upload

--- Dub.go
+++ Dub.go
@@ -2,9 +2,8 @@ package main
 
 import (
 
-	"html/template"
 	"net/http"
-	"github.com/echoofthepast/Dub/handlers"
+	"github.com/creator/Dub/handlers"
 )
 
 // Go Gophers out there will need a starting point premade for them having solved a thousand problems.
@@ -12,22 +11,11 @@ import (
 func main() {
 
 	//type localhost:1337/index in your browsers address bar
-	http.HandleFunc("/index", handlers.IndexHandler)
-	//type localhost:1337/input in your browsers address bar
-	http.HandleFunc("/input", handlers.InputHandler)
-
+	http.HandleFunc("/login", handlers.LoginHandler)
 	//parses the entire static folder and sub folders
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-
 	http.ListenAndServe(":1337", nil)
 	//end of main function calling ListenAndServe to produce output to browser.
 	//listening on port:1337 please open browser with link localhost:1337/index to view site.
 }
 
-//HandleHome is the Handler for the Home file
-func HandleHome(w http.ResponseWriter, r *http.Request) {
-
-	home := template.Must(template.ParseFiles("templates/home.html"))
-	home.Execute(w, nil)
-
-}
